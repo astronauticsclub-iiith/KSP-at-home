1c0cd125fc3107ef5d5e2adaf98a895138944d6c
satkarjuneja <satkarjuneja@gmail.com>
2026-05-12 22:40:27 +0530

    ADD lil-gui controls and imporved buttons

--- src/maneuver.js
+++ src/maneuver.js
@@ -1,17 +1,32 @@
+import GUI from 'lil-gui'
+
+const params = {
+    G: 7, // Graviational constant in ARBITRARY UNITS
+    dt: 0.01, // TIME SCALE
+    moonMass: 20/81,
+    thrust: 0.2
+};
+
+const gui = new GUI();
+
+gui.add(params, 'G', 0, 20, 0.1);
+
+gui.add(params, 'params.dt', 0.001, 0.05, 0.001);
+
+gui.add(params, 'moonMass', 0.01, 1, 0.01); // mass of earth is 1
+
+
 let r = { x:-3, y: -2, z: 0 } // postion and velocities
 let v = { x: -2, y: 0, z: 0 }
-let theta = 0;
-let G = 7; // gravitational constant times mass ARBITRARY UNITS
-let dt = 0.01 // time step
 
 let bodies = {
     earth: { m: 1, pos: { x: -3, y: -4, z: 0 } },
-    moon: { m: 20 / 81, pos: { x: 6, y: 8, z: 0 } }
+    moon: { m: params.moonMass, pos: { x: 6, y: 8, z: 0 } }
 } //artifically increase moons pull for now
 export {bodies}
 
-function distance(x1, y1, z1, x2, y2, z2) {
-    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+function distance(x1, y1,x2, y2) {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 }
 
 function acc(pos) {
@@ -24,9 +39,9 @@ function acc(pos) {
         const dx = body.pos.x - pos.x;
         const dy = body.pos.y - pos.y;
 
-        const dist = Math.sqrt(dx * dx + dy * dy);
+        const dist=distance(body.pos.x,body.pos.y,pos.x,pos.y);
 
-        const factor = G * body.m / (dist ** 3);
+        const factor = params.G * body.m / (dist ** 3);
 
         ax += factor * dx;
         ay += factor * dy;
@@ -34,6 +49,10 @@ function acc(pos) {
 
     return { ax, ay };
 }
+
+let prograding=false;
+let retrograding=false;
+
 // Implementing velocity verlet algorithm which is a symplectic integrator
 export function step() {
 
@@ -41,19 +60,26 @@ export function step() {
     const { ax: ax_old, ay: ay_old } = acc(r);
 
     // position update
-    r.x += v.x * dt + 0.5 * ax_old * dt * dt;
-    r.y += v.y * dt + 0.5 * ay_old * dt * dt;
+    r.x += v.x * params.dt + 0.5 * ax_old * params.dt * params.dt;
+    r.y += v.y * params.dt + 0.5 * ay_old * params.dt * params.dt;
 
     // new accn
     const { ax: ax_new, ay: ay_new } = acc(r);
 
     // velocity update
-    v.x += 0.5 * (ax_old + ax_new) * dt;
-    v.y += 0.5 * (ay_old + ay_new) * dt;
+    v.x += 0.5 * (ax_old + ax_new) * params.dt;
+    v.y += 0.5 * (ay_old + ay_new) * params.dt;
 
     // orientation
     const theta = Math.atan2(v.y, v.x);
-
+    
+    // prograde retrograde upgrade
+    if(prograding==true){
+        prograde();
+    }
+    if(retrograding==true){
+        retrograde();
+    }
     return {
         x: r.x,
         y: r.y,
@@ -65,7 +91,7 @@ export function step() {
 
 // Prograde and Retrograde 
 
-let dv = 0.2;
+let dv = 0.02;
 function prograde() {
 
     const speed =
@@ -74,21 +100,28 @@ function prograde() {
     v.y += dv * (v.y / speed);
 }
 function retrograde() {
-
+    
     const speed =
         Math.sqrt(v.x ** 2 + v.y ** 2);
 
     v.x -= dv * (v.x / speed);
     v.y -= dv * (v.y / speed);
 }
-
-
 const probtn = document.getElementById('prograde');
-probtn.addEventListener('click', (event) => {
-    prograde();
+probtn.addEventListener('pointerdown', (event) => {
+    prograding=true;
 })
 
+probtn.addEventListener('pointerup', (event) => {
+    prograding=false;
+})
+
+
 const retrobtn = document.getElementById('retrograde');
-retrobtn.addEventListener('click', (event) => {
-    retrograde();
+retrobtn.addEventListener('pointerdown', (event) => {
+    retrograding=true;
+})
+
+retrobtn.addEventListener('pointerup', (event) => {
+    retrograding=false;
 })
